7dfb8395f5e687d3b3384b1241f1857016dedfe5
Rakshit Sareen <rs5606@nyu.edu>
2018-09-14 17:23:54 -0400

    Initial Commit

---
+++ OperatingSystems/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  OperatingSystems
+//
+//  Created by Rakshit Sareen on 9/14/18.
+//  Copyright © 2018 Rakshit Sareen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
