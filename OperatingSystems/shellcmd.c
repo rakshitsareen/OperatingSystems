afb1d7df8f0907f4f31b3ba5d99f29aead7cfa3a
Rakshit Sareen <rs5606@nyu.edu>
2018-09-30 09:28:23 -0400

    refactoring

--- OperatingSystems/shellcmd.c
+++ OperatingSystems/shellcmd.c
@@ -1,5 +1,5 @@
 //
-//  main.c
+//  shellcmd.c
 //  OperatingSystems
 //
 //  Created by Rakshit Sareen on 9/14/18.
@@ -13,66 +13,69 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int execute_command(char* path, char* command, char* argument){
-    int d;
+/*
+ Run the command. Takes path, command and the argument.
+ */
+int execute(char* path, char* cmd, char* args){
+    int return_val;
     //sanitizing the command
-    command[strcspn(command, "\n")] = 0;
-    strcat(path,command);
-    if(argument == NULL){
-        d = execl(path,command,NULL,NULL);
+    cmd[strcspn(cmd, "\n")] = 0;    // removing trailing \n. because we used fgets. *THIS IS IMPARTANT IF WE ARE USING fgets()*
+    strcat(path,cmd);                   // concatenate the command to path
+    if(args == NULL){
+        return_val = execl(path,cmd,NULL,NULL);  // execute the command
     }
-    else{
+    else {
         // sanitize the argument
-        argument[strcspn(argument, "\n")] = 0;
-        d = execl(path, command, argument, NULL);
-        if(d==-1){
-            strcpy(path,"/bin/");
-            strcat(path,command);
-            d=execl(path,command,argument,NULL);
-            if(d==-1){
+        args[strcspn(args, "\n")] = 0;  // removing trailing \n. because we used fgets. *THIS IS IMPARTANT IF WE ARE USING fgets()*
+        return_val = execl(path, cmd, args, NULL);
+        if(return_val==-1){  // if the previous command did not run, then try /bin path
+            strcpy(path,"/bin/");   // concatenate '/bin' to the path
+            strcat(path,cmd);   // concatenate comand to path
+            return_val = execl(path,cmd,args,NULL);  // execute the command
+            if(return_val == -1){  // not able to run at all.
                 printf("cannot execute command\n");
             }
         }
     }
-    return d;
+    return return_val;
 }
 
 int main()
 {
-    char command[50], argument[50], path[50];
-    char ch;
-    int processid, status;
-    while(1) {        //infinite loop to receive input from user
-        printf("Please Enter Command to Execute : ");
-        if(fgets(command, 50, stdin) != NULL){
+    char cmd[50], args[50], cmd_path[50];
+    char args_or_not;
+    int PID, status;
+    while(1) {        //infinite loop to receive input from user, use ctrl + c to kill the program
+        printf("Please enter the command to execute : ");
+        if(fgets(cmd, 50, stdin) != NULL){
             printf("Enter 'y' for arguments or 'n' to not ");
-            scanf("%c", &ch);
+            scanf("%c", &args_or_not);
         }
-        else{
-            ch = 'n';
+        else {
+            args_or_not = 'n';
         }
         getchar();
         
-        if((processid=fork()) == 0){ //child process gets 0 on fork
-            if(ch=='y'){
+        if((PID = fork()) == 0) { //child process gets 0 on fork, so this gets executed only by the child process
+            if(args_or_not=='y') {      // if the user entered the yes for arguments
                 printf("Enter arguments (with hyphen : -) ");
-                if(fgets(argument, 50, stdin) != NULL){
+                if(fgets(args, 50, stdin) != NULL){
                     //getchar();
-                    strcpy(path,"/usr/bin/");
-                    execute_command(path, command, argument);
+                    strcpy(cmd_path,"/usr/bin/");
+                    execute(cmd_path, cmd, args);
                 }
             }
-            else {
-                strcpy(path,"/bin/");
-                execute_command(path, command, NULL);
+            else {  // if the user did not enter yes for the arguments
+                strcpy(cmd_path,"/bin/");
+                execute(cmd_path, cmd, NULL);
             }
             exit(0);
         }
-        else if(processid<0){ //fork error if processid negative
+        else if(PID<0){ //fork error if processid negative
             status= -1;
         }
-        else{
-            if(waitpid(processid, &status,0)!= processid) // parent process gets processid on fork
+        else {
+            if(waitpid(PID, &status,0)!= PID) // parent process gets processid on fork
                 status= -1;
         }
     }
